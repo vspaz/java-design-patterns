4a9ff566b6ecf1c9b42fffe81957870e880f2c04
vspaz <vladimir.edelman@gmail.com>
2021-09-18 22:09:42 +0200

    add 'DomesticAnimalFactory'

--- src/main/java/org/vspaz/creational/afactory/DomesticAnimalFactory.java
+++ src/main/java/org/vspaz/creational/afactory/DomesticAnimalFactory.java
@@ -1,4 +1,14 @@
 package org.vspaz.creational.afactory;
 
 public class DomesticAnimalFactory {
+    public static IAnimalae getAnimal(String species) {
+        switch (species) {
+            case "russian-blue":
+                return new RussianBlue();
+            case "dachshund":
+                return new Dachshund();
+            default:
+                throw new RuntimeException("undefined species" + species);
+        }
+    }
 }
