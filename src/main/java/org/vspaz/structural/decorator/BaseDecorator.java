fb5ad63a0c91f618720323a249d62bf9c9629939
vspaz <vladimir.edelman@gmail.com>
2021-09-28 21:33:39 +0200

    tiny style fix

--- src/main/java/org/vspaz/structural/decorator/BaseDecorator.java
+++ src/main/java/org/vspaz/structural/decorator/BaseDecorator.java
@@ -2,9 +2,11 @@ package org.vspaz.structural.decorator;
 
 abstract class BaseDecorator extends BuildingBlock {
     protected BuildingBlock buildingBlock;
+
     public void setBuildingBlock(BuildingBlock block) {
         buildingBlock = block;
     }
+
     @Override
     public String buildHouse() {
         if (buildingBlock != null) {
