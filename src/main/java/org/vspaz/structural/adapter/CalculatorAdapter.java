c125f842575aa9bc4f09d6ddb80b693990b6a1f6
vspaz <vladimir.edelman@gmail.com>
2021-09-29 20:57:12 +0200

    add adapter

---
+++ src/main/java/org/vspaz/structural/adapter/CalculatorAdapter.java
@@ -0,0 +1,12 @@
+package org.vspaz.structural.adapter;
+
+public class CalculatorAdapter {
+    public double getArea(Triangle triangle) {
+        Calculator calculator = new Calculator();
+        Rectangle rectangle = new Rectangle();
+
+        rectangle.length = triangle.base;
+        rectangle.width = 0.5 * triangle.height;
+        return calculator.getArea(rectangle);
+    }
+}
