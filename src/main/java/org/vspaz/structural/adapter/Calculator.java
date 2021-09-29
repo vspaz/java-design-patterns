137cc1d1722a6aaa6a7fc9e158435ea6edebe969
vspaz <vladimir.edelman@gmail.com>
2021-09-29 20:51:54 +0200

    add two sample classes

---
+++ src/main/java/org/vspaz/structural/adapter/Calculator.java
@@ -0,0 +1,7 @@
+package org.vspaz.structural.adapter;
+
+public class Calculator {
+    public double getArea(Rectangle rectangle) {
+        return rectangle.length * rectangle.width;
+    }
+}
---
+++ src/main/java/org/vspaz/structural/adapter/Rectangle.java
@@ -0,0 +1,6 @@
+package org.vspaz.structural.adapter;
+
+public class Rectangle {
+    public double length;
+    public double width;
+}
