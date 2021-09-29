c77ccde7f5c43212eb905dda759867cd74a993da
vspaz <vladimir.edelman@gmail.com>
2021-09-29 21:08:32 +0200

    improving test

--- src/tests/org/vspaz/structural/adapter/TestAdapter.java
+++ src/tests/org/vspaz/structural/adapter/TestAdapter.java
@@ -9,5 +9,9 @@ public class TestAdapter {
         Calculator calculator = new Calculator();
         Rectangle rectangle = new Rectangle(20, 30);
         Assert.assertEquals(calculator.getArea(rectangle), 600);
+
+        Triangle triangle = new Triangle(10, 20);
+        CalculatorAdapter calculatorAdapter = new CalculatorAdapter();
+        Assert.assertEquals(calculatorAdapter.getArea(triangle), 100);
     }
 }
