613ef5a2670af9003c5ac9b0454df14f1497c50a
vspaz <vladimir.edelman@gmail.com>
2021-09-29 20:54:17 +0200

    add another sample class

---
+++ src/main/java/org/vspaz/structural/adapter/Triangle.java
@@ -0,0 +1,12 @@
+package org.vspaz.structural.adapter;
+
+public class Triangle {
+    public double base;
+    public double height;
+
+    public Triangle(int base, int height) {
+        this.base = base;
+        this.height = height;
+
+    }
+}
