e5d9dd911bf7ce5c9d2c694701e2f6e6ce044879
vspaz <vladimir.edelman@gmail.com>
2021-09-30 20:55:06 +0200

    add color class

---
+++ src/main/java/org/vspaz/structural/facade/robot/Color.java
@@ -0,0 +1,11 @@
+package org.vspaz.structural.facade.robot;
+
+public class Color {
+    public String setDefault() {
+        return "steel";
+    }
+
+    public String setBlack() {
+        return "black";
+    }
+}
