dd8a5196dd8c0d9712a8f1170107ad7e8ba5fe1f
vspaz <vladimir.edelman@gmail.com>
2021-09-30 21:16:50 +0200

    style fixes

--- src/tests/org/vspaz/structural/facade/TestFacade.java
+++ src/tests/org/vspaz/structural/facade/TestFacade.java
@@ -9,11 +9,12 @@ public class TestFacade {
         Facade robotFacade = new Facade();
         Assert.assertEquals(
                 robotFacade.buildRobot(),
-                "'org.vspaz.structural.facade.robot.Body' robot is created;steel color set;bionic arms are set;arms added;legs added");
+                "'org.vspaz.structural.facade.robot.Body' " +
+                        "robot is created; steel color set; bionic arms are set; arms added; legs added");
 
         Assert.assertEquals(
                 robotFacade.deleteRobot(),
-                "arms deleted;legs deleted;'org.vspaz.structural.facade.robot.Body' robot is deleted"
+                "arms deleted; legs deleted; 'org.vspaz.structural.facade.robot.Body' robot is deleted"
         );
     }
 }
