003e3db7ba314e57f1321de147c08975c5bd1679
vspaz <vladimir.edelman@gmail.com>
2021-10-01 19:44:24 +0200

    switch prototype to junit5

--- src/tests/org/vspaz/creational/prototype/PrototypeTest.java
+++ src/tests/org/vspaz/creational/prototype/PrototypeTest.java
@@ -1,17 +1,19 @@
 package org.vspaz.creational.prototype;
 
-import org.junit.Test;
-import org.testng.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class PrototypeTest {
   @Test
-  public void testPrototypeOK() throws CloneNotSupportedException {
+  void testPrototypeOK() throws CloneNotSupportedException {
     Tesla tesla_1 = new Tesla("m1");
     CarBase tesla_2 = tesla_1.clone();
-    Assert.assertEquals(tesla_1.getModelName(), tesla_2.getModelName());
+    assertEquals(tesla_1.getModelName(), tesla_2.getModelName());
 
     tesla_2.setModelName("m2");
-    Assert.assertEquals(tesla_2.getModelName(), "m2");
-    Assert.assertNotEquals(tesla_1.getModelName(), tesla_2.getModelName());
+    assertEquals(tesla_2.getModelName(), "m2");
+    assertNotEquals(tesla_1.getModelName(), tesla_2.getModelName());
   }
 }
