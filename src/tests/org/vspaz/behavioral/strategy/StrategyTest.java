f4b4bd7bc5cca67ea56076f0e1ed306efc692f95
vspaz <vladimir.edelman@gmail.com>
2021-10-01 19:21:07 +0200

    switch StrategyTest to Junit5

--- src/tests/org/vspaz/behavioral/strategy/StrategyTest.java
+++ src/tests/org/vspaz/behavioral/strategy/StrategyTest.java
@@ -1,7 +1,5 @@
 package org.vspaz.behavioral.strategy;
 
-import org.junit.Test;
-import org.testng.Assert;
 import org.vspaz.behavioral.strategy.ducks.Decoy;
 import org.vspaz.behavioral.strategy.ducks.Mallard;
 import org.vspaz.behavioral.strategy.ducks.Model;
@@ -9,58 +7,61 @@ import org.vspaz.behavioral.strategy.ducks.RedHead;
 import org.vspaz.behavioral.strategy.ducks.RubberToy;
 import org.vspaz.behavioral.strategy.fly.FlyRocketPowered;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 public class StrategyTest {
     @Test
     public void testMallard() {
         Mallard mallard = new Mallard();
-        Assert.assertEquals(mallard.describe(), "I am a mallard duck");
-        Assert.assertEquals(mallard.swim(), "All ducks float, even decoys");
-        Assert.assertEquals(mallard.doQuack(), "Quack");
-        Assert.assertEquals(mallard.doFly(), "I'm flying");
+        assertEquals(mallard.describe(), "I am a mallard duck");
+        assertEquals(mallard.swim(), "All ducks float, even decoys");
+        assertEquals(mallard.doQuack(), "Quack");
+        assertEquals(mallard.doFly(), "I'm flying");
     }
 
     @Test
     public void testRubberDuck() {
         RubberToy rubberToy = new RubberToy();
-        Assert.assertEquals(rubberToy.describe(), "I am a rubber toy duck");
-        Assert.assertEquals(rubberToy.swim(), "All ducks float, even decoys");
-        Assert.assertEquals(rubberToy.doQuack(), "Squeak");
-        Assert.assertEquals(rubberToy.doFly(), "can't fly");
+        assertEquals(rubberToy.describe(), "I am a rubber toy duck");
+        assertEquals(rubberToy.swim(), "All ducks float, even decoys");
+        assertEquals(rubberToy.doQuack(), "Squeak");
+        assertEquals(rubberToy.doFly(), "can't fly");
     }
 
     @Test
     public void testDecoyDuck() {
         Decoy decoy = new Decoy();
-        Assert.assertEquals(decoy.describe(), "I'm a decoy duck");
-        Assert.assertEquals(decoy.swim(), "All ducks float, even decoys");
-        Assert.assertEquals(decoy.doQuack(), "Silence");
-        Assert.assertEquals(decoy.doFly(), "can't fly");
+        assertEquals(decoy.describe(), "I'm a decoy duck");
+        assertEquals(decoy.swim(), "All ducks float, even decoys");
+        assertEquals(decoy.doQuack(), "Silence");
+        assertEquals(decoy.doFly(), "can't fly");
     }
 
     @Test
     public void testRedHead() {
         RedHead readHead = new RedHead();
-        Assert.assertEquals(readHead.describe(), "I am a real read head duck");
-        Assert.assertEquals(readHead.swim(), "All ducks float, even decoys");
-        Assert.assertEquals(readHead.doQuack(), "Quack");
-        Assert.assertEquals(readHead.doFly(), "I'm flying");
+        assertEquals(readHead.describe(), "I am a real read head duck");
+        assertEquals(readHead.swim(), "All ducks float, even decoys");
+        assertEquals(readHead.doQuack(), "Quack");
+        assertEquals(readHead.doFly(), "I'm flying");
     }
 
     @Test
     public void testModelDuck() {
         Model model = new Model();
-        Assert.assertEquals(model.describe(), "I am a model duck");
-        Assert.assertEquals(model.swim(), "All ducks float, even decoys");
-        Assert.assertEquals(model.doQuack(), "Quack");
-        Assert.assertEquals(model.doFly(), "can't fly");
+        assertEquals(model.describe(), "I am a model duck");
+        assertEquals(model.swim(), "All ducks float, even decoys");
+        assertEquals(model.doQuack(), "Quack");
+        assertEquals(model.doFly(), "can't fly");
     }
 
     @Test
     public void testModelDuckRedefined() {
         Model model = new Model();
-        Assert.assertEquals(model.doFly(), "can't fly");
+        assertEquals(model.doFly(), "can't fly");
 
         model.setFlyBehavior(new FlyRocketPowered());
-        Assert.assertEquals(model.doFly(), "I'm flying with a rocket");
+        assertEquals(model.doFly(), "I'm flying with a rocket");
     }
 }
