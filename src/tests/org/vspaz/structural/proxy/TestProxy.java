a75dde3d8804c2c5e969c56d7b5a1e0e5190fa5d
vspaz <vladimir.edelman@gmail.com>
2021-10-01 19:48:54 +0200

    fix TestProxy

--- src/tests/org/vspaz/structural/proxy/TestProxy.java
+++ src/tests/org/vspaz/structural/proxy/TestProxy.java
@@ -1,12 +1,13 @@
 package org.vspaz.structural.proxy;
 
-import org.junit.Test;
-import org.testng.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestProxy {
   @Test
-  public void testProxyOk() {
+  void testProxyOk() {
     Proxy worker = new Proxy();
-    Assert.assertEquals("this is task worker", worker.doWork());
+    assertEquals("this is task worker", worker.doWork());
   }
 }
