30a22cac334386382809639fee48ceae2cd5e278
vspaz <vladimir.edelman@gmail.com>
2021-10-01 19:39:47 +0200

    switch testBuilder to junit5

--- src/tests/org/vspaz/creational/builder/TestBuilder.java
+++ src/tests/org/vspaz/creational/builder/TestBuilder.java
@@ -1,18 +1,19 @@
 package org.vspaz.creational.builder;
 
-import org.junit.Test;
-import org.testng.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestBuilder {
   @Test
-  public void testBuilderOk() {
+  void testBuilderOk() {
     Car car = new Car("Corvette");
     car.Body("carbon");
     car.Wheels(4);
     car.Headlights("neon");
 
     String corvette = car.Build();
-    Assert.assertEquals(
+    assertEquals(
         corvette,
         "Car model is 'Corvette', chassis 'carbon', 'has '4' wheels, headlights type is 'neon'.");
   }
