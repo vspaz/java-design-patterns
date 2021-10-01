521be4923a5cc5e2294b0cb152c1308c028468da
vspaz <vladimir.edelman@gmail.com>
2021-10-01 19:48:07 +0200

    fix testFacade

--- src/tests/org/vspaz/structural/decorator/TestDecorator.java
+++ src/tests/org/vspaz/structural/decorator/TestDecorator.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDecorator {
   @Test
-  public void testDecoratorOk() {
+  void testDecoratorOk() {
     Block block = new Block();
     assertEquals(block.buildHouse(), "house is built");
 
--- src/tests/org/vspaz/structural/facade/TestFacade.java
+++ src/tests/org/vspaz/structural/facade/TestFacade.java
@@ -1,18 +1,19 @@
 package org.vspaz.structural.facade;
 
-import org.junit.Test;
-import org.testng.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestFacade {
   @Test
-  public void testFacadeOk() {
+  void testFacadeOk() {
     Facade robotFacade = new Facade();
-    Assert.assertEquals(
+    assertEquals(
         robotFacade.buildRobot(),
         "'org.vspaz.structural.facade.robot.Body' "
             + "robot is created; steel color set; bionic arms are set; arms added; legs added");
 
-    Assert.assertEquals(
+    assertEquals(
         robotFacade.deleteRobot(),
         "arms deleted; legs deleted; 'org.vspaz.structural.facade.robot.Body' robot is deleted");
   }
