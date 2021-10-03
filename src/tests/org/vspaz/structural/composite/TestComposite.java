1a27ab5620c0498bbcfb76583640568c040bfe3d
vspaz <vladimir.edelman@gmail.com>
2021-10-03 19:27:14 +0200

    add test class

---
+++ src/tests/org/vspaz/structural/composite/TestComposite.java
@@ -0,0 +1,12 @@
+package org.vspaz.structural.composite;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestComposite {
+    @Test
+    void testCompositeOk() {
+
+    }
+}
