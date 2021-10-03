c57ddfbeb4d25006f138d55955f8bfdcd5edb074
vspaz <vladimir.edelman@gmail.com>
2021-10-03 20:33:34 +0200

    extend the test case

--- src/tests/org/vspaz/structural/composite/TestComposite.java
+++ src/tests/org/vspaz/structural/composite/TestComposite.java
@@ -9,23 +9,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TestComposite {
   @Test
   void testCompositeOk() {
     CompositeEmployee EngineeringManagerUI = new CompositeEmployee("UI Engineering Manager", "UI");
-    CompositeEmployee engineeringManagerBackend =
-        new CompositeEmployee("Backend Engineering Manager", "backend");
-
     IEmployee frontendDev_1 = new Employee("dev_1", "UI");
     IEmployee frontendDev_2 = new Employee("dev_2", "UI");
-
     EngineeringManagerUI.addEmployee(frontendDev_1);
     EngineeringManagerUI.addEmployee(frontendDev_2);
 
+    CompositeEmployee engineeringManagerBackend =
+        new CompositeEmployee("Backend Engineering Manager", "backend");
     IEmployee backendDev_1 = new Employee("dev_3", "backend");
     IEmployee backendDev_2 = new Employee("dev_4", "backend");
-
     engineeringManagerBackend.addEmployee(backendDev_1);
     engineeringManagerBackend.addEmployee(backendDev_2);
 
@@ -42,5 +40,11 @@ public class TestComposite {
     assertEquals(
         engineeringManagerBackend.getHierarchy(),
         new ArrayList<>(Arrays.asList(backendDev_1, backendDev_2)));
+
+    assertNull(frontendDev_1.getHierarchy());
+    assertNull(frontendDev_2.getHierarchy());
+
+    assertNull(backendDev_1.getHierarchy());
+    assertNull(backendDev_2.getHierarchy());
   }
 }
