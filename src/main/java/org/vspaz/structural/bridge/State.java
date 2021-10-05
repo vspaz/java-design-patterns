b645415008cfd714c645604317fc072cfd356af2
vspaz <vladimir.edelman@gmail.com>
2021-10-05 19:56:25 +0200

    fix interface; add StateOff class

--- src/main/java/org/vspaz/structural/bridge/State.java
+++ src/main/java/org/vspaz/structural/bridge/State.java
@@ -1,4 +1,7 @@
 package org.vspaz.structural.bridge;
 
 public interface State {
+  String switchState();
+
+  String buttonPressed();
 }
---
+++ src/main/java/org/vspaz/structural/bridge/StateOn.java
@@ -0,0 +1,13 @@
+package org.vspaz.structural.bridge;
+
+public class StateOn implements State {
+  @Override
+  public String switchState() {
+    return "state 'ON'";
+  }
+
+  @Override
+  public String buttonPressed() {
+    return "'ON' is already activate";
+  }
+}
