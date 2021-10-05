50ce6981af58fd66b1d33e361cc54b96c95b8b81
vspaz <vladimir.edelman@gmail.com>
2021-10-05 20:44:30 +0200

    style fixes

---
+++ src/main/java/org/vspaz/structural/bridge/states/devices/DvdPlayer.java
@@ -0,0 +1,9 @@
+package org.vspaz.structural.bridge.states.devices;
+
+import org.vspaz.structural.bridge.states.State;
+
+public class DvdPlayer extends Device {
+  public DvdPlayer(State state) {
+    super(state);
+  }
+}
---
+++ src/main/java/org/vspaz/structural/bridge/states/devices/TvSet.java
@@ -0,0 +1,9 @@
+package org.vspaz.structural.bridge.states.devices;
+
+import org.vspaz.structural.bridge.states.State;
+
+public class TvSet extends Device {
+  public TvSet(State state) {
+    super(state);
+  }
+}
