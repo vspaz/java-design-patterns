b999584d3270f2192c5f6e3336d14f58dba479d0
vspaz <vladimir.edelman@gmail.com>
2021-10-06 21:32:36 +0200

    add test

---
+++ src/tests/org/vspaz/behavioral/visitor/TestVisitor.java
@@ -0,0 +1,13 @@
+package org.vspaz.behavioral.visitor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestVisitor {
+    @Test
+    void testVisitorOk() {
+
+    }
+}
