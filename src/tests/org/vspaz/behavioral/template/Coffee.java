cbb0521987e8e1b8b8c324866bd0ec9ecfd0ab94
vspaz <vladimir.edelman@gmail.com>
2021-10-09 20:45:57 +0200

    extend the template class

--- src/tests/org/vspaz/behavioral/template/Coffee.java
+++ src/tests/org/vspaz/behavioral/template/Coffee.java
@@ -1,3 +1,16 @@
 package org.vspaz.behavioral.template;
 
-public class Coffee extends CaffeineDrink {}
+public class Coffee extends CaffeineDrink {
+  @Override
+  public String prepareRecipe() {
+    return super.prepareRecipe();
+  }
+
+  public String brewCoffeeGrinds() {
+    return "coffee's ground and brewed";
+  }
+
+  public String addSugarAndMilk() {
+    return "sugar and milk added to coffee";
+  }
+}
