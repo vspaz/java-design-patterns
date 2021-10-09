08dbb37e9b23eaf74381e0bf94647d1ed8002ff4
vspaz <vladimir.edelman@gmail.com>
2021-10-09 20:40:22 +0200

    add abstract class

---
+++ src/tests/org/vspaz/behavioral/template/CaffeineDrink.java
@@ -0,0 +1,15 @@
+package org.vspaz.behavioral.template;
+
+public abstract class CaffeineDrink {
+  public String prepareRecipe() {
+    return "abstract drink recipe";
+  }
+
+  public String boilWater() {
+    return "water boiled for abstract drink";
+  }
+
+  public String pourInCup() {
+    return "abstract drink is poured in the cup";
+  }
+}
