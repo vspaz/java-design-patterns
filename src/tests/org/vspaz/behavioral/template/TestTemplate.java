eb4543383a7920f1b4c6dbe359520dd14647105b
vspaz <vladimir.edelman@gmail.com>
2021-10-09 21:11:59 +0200

    add basic check to testcase

--- src/tests/org/vspaz/behavioral/template/TestTemplate.java
+++ src/tests/org/vspaz/behavioral/template/TestTemplate.java
@@ -2,9 +2,12 @@ package org.vspaz.behavioral.template;
 
 import org.junit.jupiter.api.Test;
 
-public class TestTemplate {
-    @Test
-    void testTemplateOk() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    }
+public class TestTemplate {
+  @Test
+  void testTemplateOk() {
+    Coffee coffee = new Coffee();
+    assertEquals("water boiled for abstract drink", coffee.boilWater());
+  }
 }
