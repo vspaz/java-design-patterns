c84bf4cdc47eafd804ef228265bfc66355854963
vspaz <vladimir.edelman@gmail.com>
2021-10-10 10:59:57 +0200

    extend Undo class

--- src/main/java/org/vspaz/behavioral/command/Undo.java
+++ src/main/java/org/vspaz/behavioral/command/Undo.java
@@ -1,8 +1,14 @@
 package org.vspaz.behavioral.command;
 
 public class Undo implements ICommand {
+  private final Receiver receiver;
+
+  public Undo(Receiver receiver) {
+    this.receiver = receiver;
+  }
+
   @Override
   public String runCommand() {
-    return null;
+    return receiver.runUndo();
   }
 }
