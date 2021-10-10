f48e7682aa090bf90a80610b36c9b83a0671c8a5
vspaz <vladimir.edelman@gmail.com>
2021-10-10 10:56:38 +0200

    add Receiver class

---
+++ src/main/java/org/vspaz/behavioral/command/Receiver.java
@@ -0,0 +1,11 @@
+package org.vspaz.behavioral.command;
+
+public class Receiver {
+  public String runUndo() {
+    return "'undo' run by receiver";
+  }
+
+  public String runRedo() {
+    return "'redo' run by receiver";
+  }
+}
