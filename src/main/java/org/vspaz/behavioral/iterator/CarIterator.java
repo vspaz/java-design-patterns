42624a34c01cfae6a32111e6edd5b64c8946904a
vspaz <vladimir.edelman@gmail.com>
2021-10-11 20:31:41 +0200

    interim commit

--- src/main/java/org/vspaz/behavioral/iterator/CarIterator.java
+++ src/main/java/org/vspaz/behavioral/iterator/CarIterator.java
@@ -1,2 +1,29 @@
-package org.vspaz.behavioral.iterator;public class CarIterator {
+package org.vspaz.behavioral.iterator;
+
+public class CarIterator implements Iterator {
+  private final String[] cars;
+  private final int position;
+
+  public CarIterator(String[] cars) {
+    this.cars = cars;
+    this.position = 0;
+  }
+
+  @Override
+  public void first() {}
+
+  @Override
+  public String next() {
+    return null;
+  }
+
+  @Override
+  public String getCurrent() {
+    return null;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return false;
+  }
 }
--- src/main/java/org/vspaz/behavioral/iterator/Garage.java
+++ src/main/java/org/vspaz/behavioral/iterator/Garage.java
@@ -1,8 +1,10 @@
 package org.vspaz.behavioral.iterator;
 
 public class Garage implements IVehicle {
+  private final String[] cars = new String[] {"audi", "bmw", "mercedes", "tesla"};
+
   @Override
   public Iterator createIterator() {
-    return null;
+    return new CarIterator(cars);
   }
 }
