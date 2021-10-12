cf25903eaad6dece2cd9d9d8fd033ecfbb02f664
vspaz <vladimir.edelman@gmail.com>
2021-10-12 20:27:14 +0200

    add test

--- src/tests/org/vspaz/behavioral/memento/TestMemento.java
+++ src/tests/org/vspaz/behavioral/memento/TestMemento.java
@@ -1,4 +1,20 @@
 package org.vspaz.behavioral.memento;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestMemento {
+  @Test
+  void testMementoOk() {
+    Originator originator = new Originator();
+    originator.setId(10);
+    assertEquals(10, originator.getId());
+    Memento memento = originator.save(originator.getId());
+    originator.setId(100500);
+    assertEquals(100500, originator.getId());
+    originator.revert(memento);
+
+    assertEquals(originator.getId(), 10);
+  }
 }
