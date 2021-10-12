f208dfebcb18463b82779448c19172a3f18ec604
vspaz <vladimir.edelman@gmail.com>
2021-10-12 19:22:33 +0200

    extend the Memento class

--- src/main/java/org/vspaz/behavioral/memento/Memento.java
+++ src/main/java/org/vspaz/behavioral/memento/Memento.java
@@ -1,4 +1,18 @@
 package org.vspaz.behavioral.memento;
 
 public class Memento {
+
+  private int id;
+
+  public Memento(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
 }
