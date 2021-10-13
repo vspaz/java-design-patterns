9689d171251e6066839b8e2a29b041c51418daf7
vspaz <vladimir.edelman@gmail.com>
2021-10-13 22:53:03 +0200

    extend 'Device' class

--- src/main/java/org/vspaz/behavioral/state/Device.java
+++ src/main/java/org/vspaz/behavioral/state/Device.java
@@ -3,7 +3,9 @@ package org.vspaz.behavioral.state;
 public class Device {
   private IState currentState;
 
-  public Device() {}
+  public Device() {
+    this.setCurrentState(new Off());
+  }
 
   public IState getCurrentState() {
     return currentState;
@@ -11,5 +13,17 @@ public class Device {
 
   public void setCurrentState(IState currentState) {
     this.currentState = currentState;
+  }
+
+  public String pressOffButton() {
+    return currentState.pressOffButton(this);
+  }
+
+  public String pressOnbutton() {
+    return currentState.pressOnButton(this);
+  }
+
+  public String pressMuteButton() {
+    return currentState.pressMuteButton(this);
   }
 }
--- src/tests/org/vspaz/behavioral/state/TestState.java
+++ src/tests/org/vspaz/behavioral/state/TestState.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 public class TestState {
     @Test
     void testStateOk() {
-
+        Device device = new Device();
+        device.
     }
 }
