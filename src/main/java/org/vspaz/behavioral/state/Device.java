1fe604752174c38c5f096f807d7f5e1afd144ab9
vspaz <vladimir.edelman@gmail.com>
2021-10-13 22:34:40 +0200

    extend Off and On classes

--- src/main/java/org/vspaz/behavioral/state/Device.java
+++ src/main/java/org/vspaz/behavioral/state/Device.java
@@ -1,16 +1,15 @@
 package org.vspaz.behavioral.state;
 
 public class Device {
-    private IState currentState;
-    public Device() {
+  private IState currentState;
 
-    }
+  public Device() {}
 
-    public IState getCurrentState() {
-        return currentState;
-    }
+  public IState getCurrentState() {
+    return currentState;
+  }
 
-    public void setCurrentState(IState currentState) {
-        this.currentState = currentState;
-    }
+  public void setCurrentState(IState currentState) {
+    this.currentState = currentState;
+  }
 }
--- src/main/java/org/vspaz/behavioral/state/On.java
+++ src/main/java/org/vspaz/behavioral/state/On.java
@@ -3,16 +3,20 @@ package org.vspaz.behavioral.state;
 public class On implements IState {
   @Override
   public String pressOnButton(Device ctx) {
-    return null;
+    return "'On' button pressed; device is already on; 'On' ignored.";
   }
 
   @Override
   public String pressOffButton(Device ctx) {
-    return null;
+    ctx.setCurrentState(new Off());
+    return "'Off' button pressed; switching from 'On' to 'Off': "
+        + ctx.getCurrentState().toString();
   }
 
   @Override
   public String pressMuteButton(Device ctx) {
-    return null;
+    ctx.setCurrentState(new Mute());
+    return "'Mute' button pressed; switching from 'On' to 'Mute': "
+        + ctx.getCurrentState().toString();
   }
 }
