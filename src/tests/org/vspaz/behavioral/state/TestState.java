32ffee23071e1ac7c6cc09cf31fe172f500128af
vspaz <vladimir.edelman@gmail.com>
2021-10-13 23:16:55 +0200

    improve the test

--- src/tests/org/vspaz/behavioral/state/TestState.java
+++ src/tests/org/vspaz/behavioral/state/TestState.java
@@ -8,11 +8,18 @@ public class TestState {
   @Test
   void testStateOk() {
     Device device = new Device();
-    assertEquals(
-        "'Off' button pressed; device is already off; 'Off' ignored.", device.pressOffButton());
+    String initialState = "'Off' button pressed; device is already off; 'Off' ignored.";
+    assertEquals(initialState, device.pressOffButton());
     assertEquals("device's off.", device.getCurrentState().toString());
 
-    assertEquals("'On' button pressed; switching from 'Off' to 'On': device's on.", device.pressOnbutton());
-    assertEquals("'Mute' button pressed; switching from 'On' to 'Mute': device's muted.", device.pressMuteButton());
+    assertEquals(
+        "'On' button pressed; switching from 'Off' to 'On': device's on.", device.pressOnbutton());
+    assertEquals(
+        "'Mute' button pressed; switching from 'On' to 'Mute': device's muted.",
+        device.pressMuteButton());
+    assertEquals(
+        "'Off' button pressed; switching from 'Mute' to 'Off': device's off.",
+        device.pressOffButton());
+    assertEquals(initialState, device.pressOffButton());
   }
 }
