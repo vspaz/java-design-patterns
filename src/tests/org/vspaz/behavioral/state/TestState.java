cd1587b1a4650896d4e88ce1d129743a5dffae0f
vspaz <vladimir.edelman@gmail.com>
2021-10-13 22:55:00 +0200

    add use case

--- src/tests/org/vspaz/behavioral/state/TestState.java
+++ src/tests/org/vspaz/behavioral/state/TestState.java
@@ -2,10 +2,13 @@ package org.vspaz.behavioral.state;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestState {
-    @Test
-    void testStateOk() {
-        Device device = new Device();
-        device.
-    }
+  @Test
+  void testStateOk() {
+    Device device = new Device();
+    assertEquals(
+        "'Off' button pressed; device is already off; 'Off' ignored.", device.pressOffButton());
+  }
 }
