fd95d3aaffb699d3ece2676f5f23a0e7d7d7f2e6
vspaz <vladimir.edelman@gmail.com>
2021-10-13 19:40:20 +0200

    add class 'Device'

---
+++ src/main/java/org/vspaz/behavioral/state/Device.java
@@ -0,0 +1,16 @@
+package org.vspaz.behavioral.state;
+
+public class Device {
+    private IState currentState;
+    public Device() {
+
+    }
+
+    public IState getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(IState currentState) {
+        this.currentState = currentState;
+    }
+}
