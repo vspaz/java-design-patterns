0a4c9afc1126e093289ebd9f7738ae45830d5ceb
vspaz <vladimir.edelman@gmail.com>
2021-10-13 19:45:05 +0200

    add 'On' class

---
+++ src/main/java/org/vspaz/behavioral/state/On.java
@@ -0,0 +1,18 @@
+package org.vspaz.behavioral.state;
+
+public class On implements IState {
+  @Override
+  public String pressOnButton(Device ctx) {
+    return null;
+  }
+
+  @Override
+  public String pressOffButton(Device ctx) {
+    return null;
+  }
+
+  @Override
+  public String pressMuteButton(Device ctx) {
+    return null;
+  }
+}
