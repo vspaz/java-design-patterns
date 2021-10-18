a63ba00f465e4320c58f2cfa3e670b2dc2015667
vspaz <vladimir.edelman@gmail.com>
2021-10-18 22:40:50 +0200

    add interim commit

--- src/tests/org/vspaz/behavioral/chor/TestChainOfResponsibility.java
+++ src/tests/org/vspaz/behavioral/chor/TestChainOfResponsibility.java
@@ -1,11 +1,16 @@
 package org.vspaz.behavioral.chor;
 
-
 import org.junit.jupiter.api.Test;
 
 public class TestChainOfResponsibility {
-    @Test
-    void testChainOfResponsibilityOk() {
+  @Test
+  void testChainOfResponsibilityOk() {
+    IssueHandler issueHandler = new IssueHandler();
+    FaxErrorHandler faxErrorHandler = new FaxErrorHandler();
+    EmailErrorHandler emailErrorHandler = new EmailErrorHandler();
 
-    }
+    issueHandler.handleNextError(faxErrorHandler);
+    faxErrorHandler.nextErrorHandler(emailErrorHandler);
+    emailErrorHandler.nextErrorHandler(null);
+  }
 }
