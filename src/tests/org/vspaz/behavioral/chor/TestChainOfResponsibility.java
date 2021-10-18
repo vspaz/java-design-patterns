53cc0265e6be4c812d43c23648075b76db0397c0
vspaz <vladimir.edelman@gmail.com>
2021-10-18 22:51:37 +0200

    interim commit

--- src/tests/org/vspaz/behavioral/chor/TestChainOfResponsibility.java
+++ src/tests/org/vspaz/behavioral/chor/TestChainOfResponsibility.java
@@ -12,5 +12,15 @@ public class TestChainOfResponsibility {
     issueHandler.handleNextError(faxErrorHandler);
     faxErrorHandler.nextErrorHandler(emailErrorHandler);
     emailErrorHandler.nextErrorHandler(null);
+
+    Message message_1 = new Message("fax is slow", MessagePriority.HIGH);
+    Message message_2 = new Message("email aren't reaching", MessagePriority.HIGH);
+    Message message_3 = new Message("cc disabled", MessagePriority.NORMAL);
+    Message message_4 = new Message("fax never reach", MessagePriority.HIGH);
+
+    issueHandler.raiseError(message_1);
+    issueHandler.raiseError(message_2);
+    issueHandler.raiseError(message_3);
+    issueHandler.raiseError(message_3);
   }
 }
