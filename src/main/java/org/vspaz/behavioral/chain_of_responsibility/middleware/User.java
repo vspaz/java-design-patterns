31210f92fc1fe548dd300a46af3147f80a6785bc
vspaz <vladimir.edelman@gmail.com>
2021-10-19 23:47:29 +0200

    add empty test case

--- src/main/java/org/vspaz/behavioral/chain_of_responsibility/middleware/User.java
+++ src/main/java/org/vspaz/behavioral/chain_of_responsibility/middleware/User.java
@@ -1,8 +1,22 @@
 package org.vspaz.behavioral.chain_of_responsibility.middleware;
 
+import org.vspaz.behavioral.chain_of_responsibility.server.Server;
+
 public class User extends Base {
-    @Override
-    public String check(String email, String password) {
-        return null;
+  private final Server server;
+
+  public User(Server server) {
+    this.server = server;
+  }
+
+  @Override
+  public String check(String email, String password) {
+    if (!server.hasEmail(email)) {
+      return "email not found";
     }
+    if (!server.isPasswordValid(email, password)) {
+      return "password incorrect";
+    }
+    return checkNext(email, password);
+  }
 }
---
+++ src/tests/org/vspaz/behavioral/chain_of_responsibility/TestChainOfResponsibility.java
@@ -0,0 +1,10 @@
+package org.vspaz.behavioral.chain_of_responsibility;
+
+import org.junit.jupiter.api.Test;
+
+public class TestChainOfResponsibility {
+    @Test
+    void testChainOfResponsibilityOk() {
+
+    }
+}
