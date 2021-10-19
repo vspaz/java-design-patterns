de6da837a203a723e82a1a159404f38149e2ce89
vspaz <vladimir.edelman@gmail.com>
2021-10-19 21:30:45 +0200

    ThrottlingMwe

--- src/main/java/org/vspaz/behavioral/chain_of_responsibility/middleware/ThrottlingMwe.java
+++ src/main/java/org/vspaz/behavioral/chain_of_responsibility/middleware/ThrottlingMwe.java
@@ -1,2 +1,27 @@
-package org.vspaz.behavioral.chain_of_responsibility.middleware;public class ThrottlingMwe {
+package org.vspaz.behavioral.chain_of_responsibility.middleware;
+
+public class ThrottlingMwe extends MweBase {
+
+  private final int rps;
+  private int requestCount;
+  private long currentTime;
+
+  public ThrottlingMwe(int rps) {
+    this.rps = rps;
+    this.currentTime = System.currentTimeMillis();
+  }
+
+  @Override
+  public String check(String email, String password) {
+    if (System.currentTimeMillis() > currentTime + 60_000) {
+      this.requestCount = 0;
+      currentTime = System.currentTimeMillis();
+    }
+    requestCount++;
+
+    if (requestCount > rps) {
+      return "rps limit exceeded";
+    }
+    return checkNext(email, password);
+  }
 }
