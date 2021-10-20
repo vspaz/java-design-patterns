1e6583033e3480f0ba1401ae2f17b7beb6c1e06c
vspaz <vladimir.edelman@gmail.com>
2021-10-20 21:31:07 +0200

    extend 'Context' class

--- src/main/java/org/vspaz/behavioral/interpreter/Context.java
+++ src/main/java/org/vspaz/behavioral/interpreter/Context.java
@@ -1,4 +1,24 @@
 package org.vspaz.behavioral.interpreter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Context {
+  private final int workExperienceInYears;
+  private final List<String> permissionGrades;
+
+  public Context(int workExperienceInYears, String... allowedGrades) {
+    this.workExperienceInYears = workExperienceInYears;
+    this.permissionGrades = new ArrayList<>();
+    permissionGrades.addAll(Arrays.asList(allowedGrades));
+  }
+
+  public int getWorkExperienceInYears() {
+    return workExperienceInYears;
+  }
+
+  public List<String> getPermissionGrades() {
+    return permissionGrades;
+  }
 }
