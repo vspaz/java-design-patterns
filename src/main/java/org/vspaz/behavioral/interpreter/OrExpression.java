c905f11873e73f4d3367dc78b293988a88012821
vspaz <vladimir.edelman@gmail.com>
2021-10-20 22:07:41 +0200

    quick style fix

---
+++ src/main/java/org/vspaz/behavioral/interpreter/OrExpression.java
@@ -0,0 +1,8 @@
+package org.vspaz.behavioral.interpreter;
+
+public class OrExpression implements IEmployee {
+    @Override
+    public boolean interpret(Context ctx) {
+        return false;
+    }
+}
