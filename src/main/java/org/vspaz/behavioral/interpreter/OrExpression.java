a8b7aad15a56bac106ec4e9a15febf5676c3bed7
vspaz <vladimir.edelman@gmail.com>
2021-10-21 21:39:26 +0200

    add 'And' expression class

--- src/main/java/org/vspaz/behavioral/interpreter/OrExpression.java
+++ src/main/java/org/vspaz/behavioral/interpreter/OrExpression.java
@@ -1,10 +1,10 @@
 package org.vspaz.behavioral.interpreter;
 
 public class OrExpression implements IEmployee {
-  private final Employee employee_1;
-  private final Employee employee_2;
+  private final IEmployee employee_1;
+  private final IEmployee employee_2;
 
-  public OrExpression(Employee employee_1, Employee employee_2) {
+  public OrExpression(IEmployee employee_1, IEmployee employee_2) {
     this.employee_1 = employee_1;
     this.employee_2 = employee_2;
   }
---
+++ src/main/java/org/vspaz/behavioral/interpreter/expressions/And.java
@@ -0,0 +1,19 @@
+package org.vspaz.behavioral.interpreter.expressions;
+
+import org.vspaz.behavioral.interpreter.Context;
+import org.vspaz.behavioral.interpreter.IEmployee;
+
+public class And implements IEmployee {
+    private final IEmployee employee_1;
+    private final IEmployee employee_2;
+
+    public And(IEmployee employee_1, IEmployee employee_2) {
+        this.employee_1 = employee_1;
+        this.employee_2 = employee_2;
+    }
+
+    @Override
+    public boolean interpret(Context ctx) {
+        return employee_1.interpret(ctx) && employee_2.interpret(ctx);
+    }
+}
