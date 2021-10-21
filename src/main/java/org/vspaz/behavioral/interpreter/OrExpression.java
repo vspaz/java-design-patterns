9633d18be779c3e204b04d6b3722033044625a45
vspaz <vladimir.edelman@gmail.com>
2021-10-21 21:34:31 +0200

    add OrExpression

--- src/main/java/org/vspaz/behavioral/interpreter/OrExpression.java
+++ src/main/java/org/vspaz/behavioral/interpreter/OrExpression.java
@@ -1,8 +1,16 @@
 package org.vspaz.behavioral.interpreter;
 
 public class OrExpression implements IEmployee {
-    @Override
-    public boolean interpret(Context ctx) {
-        return false;
-    }
+  private final Employee employee_1;
+  private final Employee employee_2;
+
+  public OrExpression(Employee employee_1, Employee employee_2) {
+    this.employee_1 = employee_1;
+    this.employee_2 = employee_2;
+  }
+
+  @Override
+  public boolean interpret(Context ctx) {
+    return employee_1.interpret(ctx) || employee_2.interpret(ctx);
+  }
 }
