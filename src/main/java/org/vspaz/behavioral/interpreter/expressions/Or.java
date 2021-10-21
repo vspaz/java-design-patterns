dfdae29d21a4354c6f3e83efd6f4c4b51872a850
vspaz <vladimir.edelman@gmail.com>
2021-10-21 21:40:56 +0200

    move 'Or' class to expressions package

--- src/main/java/org/vspaz/behavioral/interpreter/expressions/Or.java
+++ src/main/java/org/vspaz/behavioral/interpreter/expressions/Or.java
@@ -1,10 +1,13 @@
-package org.vspaz.behavioral.interpreter;
+package org.vspaz.behavioral.interpreter.expressions;
 
-public class OrExpression implements IEmployee {
+import org.vspaz.behavioral.interpreter.Context;
+import org.vspaz.behavioral.interpreter.IEmployee;
+
+public class Or implements IEmployee {
   private final IEmployee employee_1;
   private final IEmployee employee_2;
 
-  public OrExpression(IEmployee employee_1, IEmployee employee_2) {
+  public Or(IEmployee employee_1, IEmployee employee_2) {
     this.employee_1 = employee_1;
     this.employee_2 = employee_2;
   }
