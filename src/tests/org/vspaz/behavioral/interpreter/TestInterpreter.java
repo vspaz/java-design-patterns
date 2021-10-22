fcc989b5c74bf655086166a5a5f5ed1079242664
vspaz <vladimir.edelman@gmail.com>
2021-10-22 21:22:50 +0200

    extend the class

--- src/tests/org/vspaz/behavioral/interpreter/TestInterpreter.java
+++ src/tests/org/vspaz/behavioral/interpreter/TestInterpreter.java
@@ -2,6 +2,7 @@ package org.vspaz.behavioral.interpreter;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestInterpreter {
@@ -11,6 +12,8 @@ public class TestInterpreter {
     IEmployee employee_1 = new Employee(1, "G1");
     IEmployee employee_2 = new Employee(11, "G2");
 
+    assertFalse(EmployeeFactory.getExpression(employee_1, employee_2, "and").interpret(ctx));
+    assertTrue(EmployeeFactory.getExpression(employee_1, employee_2, "not").interpret(ctx));
     assertTrue(EmployeeFactory.getExpression(employee_1, employee_2, "or").interpret(ctx));
   }
 }
